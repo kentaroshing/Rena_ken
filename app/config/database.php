bdf6bb1e086b300cdbe5678e950adbc9a128830d
Ken Rena <kentaroching@gmail.com>
2025-09-13 21:48:15 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'studentdb',
+    'username'	=> 'sql12798487',
+    'password'	=> 'LyTzFj22eD',
+    'database'	=> 'sql12798487',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
