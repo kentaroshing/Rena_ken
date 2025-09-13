48c6b90b9ab80fa4a51bcca6a9df59afa7a2f64f
Ken Rena <kentaroching@gmail.com>
2025-09-13 22:57:58 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -63,7 +63,7 @@ $database['main'] = array(
     'port'		=> '3306',
     'username'	=> 'sql12798487',
     'password'	=> 'LyTzFj22eD',
-    'database'	=> 'studentdb',
+    'database'	=> 'sql12798487',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
